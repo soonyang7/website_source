72e13b61703c1d073dc5f077382ad8c4e1014a40
Chunyang Huang <chunyangchief@outlook.com>
2018-08-14 11:15:15 +0900

    9a

--- app/auth/views.py
+++ app/auth/views.py
@@ -37,8 +37,8 @@ def register():
         db.session.add(user)
         db.session.commit()
         token = user.generate_confirmation_token()
-        send_email(user.email, 'Confirm Your Account',
-                   'auth/email/confirm', user=user, token=token)
+        # send_email(user.email, 'Confirm Your Account',
+        #            'auth/email/confirm', user=user, token=token)
         flash('A confirmation email has been sent to you by email.')
         return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
